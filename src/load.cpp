960421f9539ce1f2fa1c4e73efbdd6d08b5d4054
Ivantea <ivantea2004@gmail.com>
2021-08-25 18:21:32 +0300

    +

--- src/load.cpp
+++ src/load.cpp
@@ -27,8 +27,40 @@ namespace gl_loader
 
 	void load_impl()
 	{
+
+
+		WNDCLASSA wc;
+		wc.cbClsExtra = 0;
+		wc.cbWndExtra = 0;
+		wc.hbrBackground = 0;
+		wc.hCursor = LoadCursor(0, IDC_ARROW);
+		wc.hIcon = LoadIcon(0, IDI_APPLICATION);
+		wc.hInstance = GetModuleHandleA(0);
+		wc.lpfnWndProc = DefWindowProcW;
+		wc.lpszClassName = "gl_loader dummy window class";
+		wc.lpszMenuName = 0;
+		wc.style = 0;
+
+		RegisterClassA(&wc);
+
+		HWND hwnd = CreateWindowExA(0, wc.lpszClassName, "", 0, 0, 0, 0, 0, 0, 0, wc.hInstance, 0);
+
+		HDC hdc = GetDC(hwnd);
+
+		HGLRC ctx = wglCreateContext(hdc);
+
+		wglMakeCurrent(hdc, ctx);
+
 		//gladLoaderLoadGL();
 		gladLoaderLoadGL();
+
+
+		wglDeleteContext(ctx);
+
+
+		ReleaseDC(hwnd, hdc);
+		DestroyWindow(hwnd);
+		UnregisterClassA(wc.lpszClassName, wc.hInstance);
 	}
 
 }
